2c4d7e8021b38b11028cfff5cc30bdd020cde494
unknown <nzareckaya.nz@gmail.com>
2016-01-17 23:00:49 +0200

    Create basic structure of site

---
+++ Site/app/app.config.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    'use strict';
+    angular.module('app')
+        .config(['$urlRouterProvider', '$stateProvider',
+            function($urlRouterProvider, $stateProvider) {
+                $urlRouterProvider.otherwise('/');
+                $stateProvider
+                    .state('login', {
+                        url: '/login',
+                        templateUrl: 'app/components/Login/login.html'
+                    })
+                    .state('help', {
+                        url: '/help',
+                        templateUrl: 'app/components/Help/help.html'
+                    })
+                    .state('home', {
+                        url: '/',
+                        templateUrl: 'app/components/Home/home.html'
+                    });
+            }]);
+})();
