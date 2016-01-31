20444f67ef052836d5032d0873a5c8ab3dace911
unknown <nzareckaya.nz@gmail.com>
2016-02-01 00:26:36 +0200

    New site // footer nav lognav contacts - done

---
+++ NewSite/app/app.config.js
@@ -0,0 +1,63 @@
+﻿(function() {
+    "use strict";
+    angular.module("app")
+        .config([
+            "$urlRouterProvider", "$stateProvider",
+            function($urlRouterProvider, $stateProvider) {
+                $urlRouterProvider.otherwise("/");
+                $stateProvider
+                    .state("faqLP", {
+                        url: "/faq",
+                        templateUrl: "app/components/LP/help.html"
+                    })
+                    .state("agreementLP", {
+                        url: "/agreement",
+                        templateUrl: "app/components/LP/agreement.html"
+                    })
+                    .state("marketingLP", {
+                        url: "/marketing",
+                        templateUrl: "app/components/LP/marketing.html"
+                    })
+                    .state("homeLP", {
+                        url: "/",
+                        templateUrl: "app/components/LP/home.html"
+                    })
+                    .state("mainLK", {
+                        url: "/lk/main",
+                        templateUrl: "app/components/LK/main.html"
+                    })
+                    .state("operationsLK", {
+                        url: "/lk/operations",
+                        templateUrl: "app/components/LK/operations.html"
+                    })
+                    .state("contactsLP", {
+                        url: "/contacts",
+                        templateUrl: "app/components/LP/contacts.html"
+                    })
+                    .state("partnersLK", {
+                        url: "/lk/partners",
+                        templateUrl: "app/components/LK/partners.html"
+                    })
+                    .state("tiketiLK", {
+                        url: "/lk/tiketi",
+                        templateUrl: "app/components/LK/tiketi.html"
+                    })
+                    .state("reviewsLP", {
+                        url: "/reviews",
+                        templateUrl: "app/components/LP/reviews.html"
+                    })
+                    .state("newsLP", {
+                        url: "/news",
+                        templateUrl: "app/components/LP/news.html"
+                    })
+                    .state("optionsLK", {
+                        url: "/lk/options",
+                        templateUrl: "app/components/LK/options.html"
+                    })
+                    .state("banersLK", {
+                        url: "/lk/baners",
+                        templateUrl: "app/components/LK/baners.html"
+                    });
+            }
+        ]);
+})();
---
+++ NewSite/app/components/LK/lk.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('app').controller('LKCtrl', function ($scope, $timeout) {
+        $scope.clock = "loading clock..."; // initialise the time variable
+        $scope.tickInterval = 1000;//ms
+
+        var tick = function () {
+            $scope.clock = Date.now();// get the current time
+            $timeout(tick, $scope.tickInterval); // reset the timer
+        }
+
+        // Start the timer
+        $timeout(tick, $scope.tickInterval);
+    });
+})();
